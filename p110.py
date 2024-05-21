8a573a4138ab5643836ed91d13ae16c885f50590
Christian-Andersen <cdandersen98@gmail.com>
2024-05-21 18:17:50 +1000

    changed p110 a bit

--- p110.py
+++ p110.py
@@ -1,10 +1,11 @@
 from fractions import Fraction
-import numpy as np
+from sympy import factorint
 
-def check_f(n:int) -> int:
+
+def f(n: int):
     xs = []
     ys = []
-    y = float('inf')
+    y = float("inf")
     x = n
     while x < y:
         x += 1
@@ -15,22 +16,10 @@ def check_f(n:int) -> int:
             ys.append(y)
     return xs, ys
 
-def f(n:int) -> int:
-    x = np.arange(n+1, 2*n + 1)
-    return int(((n*x)%(x-n) == 0).sum())
-
-def old_main():
-    best = 0
-    n = 2162160
-    while True:
-        out = f(n)
-        if out > best:
-            best = out
-            print(n, best)
-        n += 2162160
 
 def main():
-    print(check_f(1260))
+    print(len(f(2162160)[0]))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
