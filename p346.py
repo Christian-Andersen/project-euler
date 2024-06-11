a986d34f57f93563ddc5f0bb1a26002b1d4dce1f
Christian-Andersen <122653077+Christian-Andersen@users.noreply.github.com>
2024-06-11 08:44:47 +0000

    .

--- p346.py
+++ p346.py
@@ -1,7 +1,7 @@
 # Strong Repunits
 
 
-def numberToBase(n, b):
+def number_to_base(n: int, b: int) -> list[int]:
     if n == 0:
         return [0]
     digits = []
@@ -11,8 +11,23 @@ def numberToBase(n, b):
     return digits[::-1]
 
 
+def is_strong_repunit(n: int) -> bool:
+    if n == 1:
+        return True
+    count = 0
+    for b in range(2, n):
+        l = number_to_base(n, b)
+        if (l[0] == 1) and (len(set(l)) == 1):
+            count += 1
+            if count == 2:
+                return True
+    return False
+
+
 def main():
-    return
+    for i in range(1, 50):
+        if is_strong_repunit(i):
+            print(i)
 
 
 if __name__ == "__main__":
